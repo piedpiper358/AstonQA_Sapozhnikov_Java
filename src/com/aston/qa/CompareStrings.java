38a62422ea401a3b70526fbebb95f298062c3799
Piedpiper <43519401+piedpiper358@users.noreply.github.com>
2024-02-29 23:30:29 +0300

    Cleaned up the code

--- src/com/aston/qa/CompareStrings.java
+++ src/com/aston/qa/CompareStrings.java
@@ -1,16 +1,13 @@
 package com.aston.qa;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class CompareStrings {
-    /*
-    2. Написать программу, которая принимает на вход две строки (a и b) и сравнивает их.
-    В результате сравнения в консоль должно быть выведено одно из сообщений: "Строки неидентичны" или "Строки идентичны"
-     */
+    /* 2. Написать программу, которая принимает на вход две строки (a и b) и сравнивает их.
+        В результате сравнения в консоль должно быть выведено одно из сообщений: "Строки неидентичны" или "Строки идентичны"
+        Плюсом будет возможность для пользователя в решении первой и второй задач ввести данные самостоятельно через консоль */
 
     public static void main(String[] args) {
-        //https://javarush.com/groups/posts/760-java-string-voprosih-k-sobesedovaniju-i-otvetih-na-nikh-ch2
 
         Scanner console = new Scanner(System.in);
         String a = console.nextLine();
--- src/com/aston/qa/PrintEvenNums.java
+++ src/com/aston/qa/PrintEvenNums.java
@@ -1,9 +1,7 @@
 package com.aston.qa;
 
 public class PrintEvenNums {
-     /*
-    3. Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Необходимо написать программу, которая выведет в консоль все чётные числа.
-    * */
+     /* 3. Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Необходимо написать программу, которая выведет в консоль все чётные числа. */
 
     public static void main(String[] args) {
         Integer[] nums = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -14,6 +12,5 @@ public class PrintEvenNums {
             }
         }
         catch (NullPointerException e){}
-
     }
 }
